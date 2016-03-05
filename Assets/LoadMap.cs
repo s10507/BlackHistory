59aa27ab2e36efdd5e0171e80ec188e8c978fbba
s10528 <yunron1020@gmail.com>
2016-03-06 02:04:36 +0900

    色々更新

---
+++ Assets/LoadMap.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMap : MonoBehaviour {
+    public TextAsset _layout;
+    public GameObject[] _objs;
+
+    // Use this for initialization
+    void Start () {
+        this.readMap();
+    }
+
+    void readMap()
+    {
+        char[] kugiri = { '\r', '\n' };
+        string[] layoutInfo = _layout.text.Split(kugiri);
+
+        string[] eachInfo;
+        for (int i = 0; i < layoutInfo.Length; i++)
+        {
+            eachInfo = layoutInfo[i].Split(","[0]);
+
+            int objNumber = getObj(eachInfo[0]);
+            GameObject obj = _objs[objNumber];
+            Vector2 pos = new Vector2(int.Parse(eachInfo[1]),
+                                      int.Parse(eachInfo[2]));
+            this.createObj(obj, pos);
+        }
+    }
+
+    int getObj(string objType)
+    {
+        int resultNum = 0;
+        switch (objType)
+        {
+            case "A":
+                resultNum = 0;
+                break;
+            case "B":
+                resultNum = 1;
+                break;
+            case "C":
+                resultNum = 2;
+                break;
+            case "D":
+                resultNum = 3;
+                break;
+            default:
+                resultNum = 0;
+                break;
+        }
+        return resultNum;
+    }
+
+    void createObj(GameObject obj, Vector2 pos)
+    {
+        GameObject go = Instantiate(obj,
+                                    new Vector3(pos.x, pos.y, 0),
+                                    obj.transform.rotation) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+}
---
+++ Assets/Script/makeDB.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeDB : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        SqliteDatabase sqlDB = new SqliteDatabase("test.db");
+        string query = "select * from userinfo";
+        DataTable db = sqlDB.ExecuteQuery(query);
+        string name = "";
+        int userId = 0;
+        foreach (DataRow dr in db.Rows)
+        {
+            name = (string)dr["user_name"];
+            userId = (int)dr["user_id"];
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
