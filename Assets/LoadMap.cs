bb40999ea77993d59c41629351f328afe168efd2
s10528 <yunron1020@gmail.com>
2016-03-06 13:01:01 +0900

    作業途中

--- Assets/LoadMap.cs
+++ Assets/LoadMap.cs
@@ -1,67 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class LoadMap : MonoBehaviour {
     public TextAsset _layout;
     public GameObject[] _objs;
-
+    public Texture texture1;
+    //public Material material1;
     // Use this for initialization
     void Start () {
-        this.readMap();
+        //texture1 = (Texture)Resources.Load("18");
+        //readMap();
+    }
+
+    void OnRenderObject()
+    {
+        char[] kugiri = { '\r', '\n' };
+        string[] layoutInfo = _layout.text.Split(kugiri);
+        string tip_name;
+        string[] eachInfo;
+        int eventtip;
+        for (int i = 0; i < layoutInfo.Length; i++)
+        {
+            eachInfo = layoutInfo[i].Split(","[0]);
+            eventtip = int.Parse(eachInfo[0].Substring(eachInfo[0].Length - 1));
+            tip_name = eachInfo[0].Substring(0, eachInfo[0].Length - 1);
+            GameObject obj = _objs[0];
+            Vector2 pos = new Vector2(int.Parse(eachInfo[1]),
+                                      int.Parse(eachInfo[2]));
+            //this.createObj(obj, pos, eventtip, tip_name);
+            Graphics.DrawTexture(new Rect(10, 10, 100, 100), texture1);
+        }
+        
     }
 
     void readMap()
     {
         char[] kugiri = { '\r', '\n' };
         string[] layoutInfo = _layout.text.Split(kugiri);
-
+        string tip_name;
         string[] eachInfo;
+        int eventtip;
         for (int i = 0; i < layoutInfo.Length; i++)
         {
             eachInfo = layoutInfo[i].Split(","[0]);
-
-            int objNumber = getObj(eachInfo[0]);
-            GameObject obj = _objs[objNumber];
+            eventtip = int.Parse(eachInfo[0].Substring(eachInfo[0].Length - 1));
+            tip_name = eachInfo[0].Substring(0, eachInfo[0].Length - 1);
+            GameObject obj = _objs[0];
             Vector2 pos = new Vector2(int.Parse(eachInfo[1]),
                                       int.Parse(eachInfo[2]));
-            this.createObj(obj, pos);
+            this.createObj(obj, pos, eventtip, tip_name);
+
         }
     }
 
-    int getObj(string objType)
-    {
-        int resultNum = 0;
-        switch (objType)
-        {
-            case "A":
-                resultNum = 0;
-                break;
-            case "B":
-                resultNum = 1;
-                break;
-            case "C":
-                resultNum = 2;
-                break;
-            case "D":
-                resultNum = 3;
-                break;
-            default:
-                resultNum = 0;
-                break;
-        }
-        return resultNum;
-    }
 
-    void createObj(GameObject obj, Vector2 pos)
+    void createObj(GameObject obj, Vector2 pos, int eventtip, string name)
     {
+        Texture2D texture = Resources.Load(name) as Texture2D;
+        Image img = GameObject.Find("Cube").GetComponent<Image>();
+        //img.sprite = Sprite.Create(texture, new Rect(pos.x, pos.y, texture.width, texture.height), Vector2.zero);
+
+
         GameObject go = Instantiate(obj,
-                                    new Vector3(pos.x, pos.y, 0),
+                                    pos,
                                     obj.transform.rotation) as GameObject;
     }
 
     // Update is called once per frame
-    void Update () {
-	
-	}
+    void Update()
+    {
+        //material1.mainTexture = texture1;
+    }
 
 }
