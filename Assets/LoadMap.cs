65ccfe04391b3f7492536fb49d5f531f78848e65
s10528 <yunron1020@gmail.com>
2016-03-06 18:32:33 +0900

    マップ作製(csvファイル読み取り実装，イベントタグ仮実装，部屋割未実装)

--- Assets/LoadMap.cs
+++ Assets/LoadMap.cs
@@ -5,73 +5,85 @@ using UnityEngine.UI;
 
 public class LoadMap : MonoBehaviour {
     public TextAsset _layout;
-    public GameObject[] _objs;
+    public GameObject _cube;
     public Texture texture1;
-    //public Material material1;
+    public Material material1;
     // Use this for initialization
     void Start () {
         //texture1 = (Texture)Resources.Load("18");
-        //readMap();
-    }
-
-    void OnRenderObject()
-    {
-        char[] kugiri = { '\r', '\n' };
-        string[] layoutInfo = _layout.text.Split(kugiri);
-        string tip_name;
-        string[] eachInfo;
-        int eventtip;
-        for (int i = 0; i < layoutInfo.Length; i++)
-        {
-            eachInfo = layoutInfo[i].Split(","[0]);
-            eventtip = int.Parse(eachInfo[0].Substring(eachInfo[0].Length - 1));
-            tip_name = eachInfo[0].Substring(0, eachInfo[0].Length - 1);
-            GameObject obj = _objs[0];
-            Vector2 pos = new Vector2(int.Parse(eachInfo[1]),
-                                      int.Parse(eachInfo[2]));
-            //this.createObj(obj, pos, eventtip, tip_name);
-            Graphics.DrawTexture(new Rect(10, 10, 100, 100), texture1);
-        }
-        
+        //material1 = Instantiate((Material)Resources.Load("tex")) as Material;
+        readMap();
     }
 
     void readMap()
     {
         char[] kugiri = { '\r', '\n' };
         string[] layoutInfo = _layout.text.Split(kugiri);
-        string tip_name;
+        string tipname;
         string[] eachInfo;
         int eventtip;
         for (int i = 0; i < layoutInfo.Length; i++)
         {
             eachInfo = layoutInfo[i].Split(","[0]);
-            eventtip = int.Parse(eachInfo[0].Substring(eachInfo[0].Length - 1));
-            tip_name = eachInfo[0].Substring(0, eachInfo[0].Length - 1);
-            GameObject obj = _objs[0];
-            Vector2 pos = new Vector2(int.Parse(eachInfo[1]),
-                                      int.Parse(eachInfo[2]));
-            this.createObj(obj, pos, eventtip, tip_name);
-
+            if (eachInfo[0].Length != 0)
+            {
+                eventtip = int.Parse(eachInfo[0].Substring(eachInfo[0].Length - 1));
+                tipname = eachInfo[0].Substring(0, eachInfo[0].Length - 1);
+                Vector2 pos = new Vector2(float.Parse(eachInfo[1]) * 32f,
+                                          float.Parse(eachInfo[2]) * 32f);
+                this.createObj(_cube, pos, eventtip, tipname);
+            }
         }
     }
 
 
     void createObj(GameObject obj, Vector2 pos, int eventtip, string name)
     {
-        Texture2D texture = Resources.Load(name) as Texture2D;
-        Image img = GameObject.Find("Cube").GetComponent<Image>();
+        //Texture2D texture = Resources.Load(name) as Texture2D;
+        //material1.mainTexture = texture1;
+        //Image img = GameObject.Find("Cube").GetComponent<Image>();
         //img.sprite = Sprite.Create(texture, new Rect(pos.x, pos.y, texture.width, texture.height), Vector2.zero);
 
-
-        GameObject go = Instantiate(obj,
+        var sr = _cube.GetComponent<SpriteRenderer>();
+        sr.sprite = Resources.Load<Sprite>(name);
+        //sr.material = material1;
+        Debug.Log(sr.sprite.bounds.size.x);
+        GameObject go = Instantiate(_cube,
                                     pos,
                                     obj.transform.rotation) as GameObject;
+        switch (eventtip)
+        {
+            //背景用チップ
+            case 0:
+                go.tag = "background";
+                break;
+            //ぶつかる壁用チップ
+            case 1:
+                go.tag = "collider";
+                break;
+            //ダメージトラップ用チップ
+            case 2:
+                go.tag = "damage";
+                break;
+            //ストーリーイベント発生用チップ
+            case 3:
+                go.tag = "moving";
+                break;
+            //画面遷移用チップ
+            case 4:
+                go.tag = "transition";
+                break;
+            //通路用チップ
+            case 5:
+                go.tag = "way";
+                break;
+        }    
     }
 
     // Update is called once per frame
     void Update()
     {
-        //material1.mainTexture = texture1;
+        
     }
 
 }
